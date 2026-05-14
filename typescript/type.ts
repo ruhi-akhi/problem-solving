44c7b4b71c341e577040adc9a94e1482a3ba1c04
akhiakter25556 <akhiakterherpower80@gmail.com>
2026-05-14 16:26:26 +0600

    2problems slove

--- typescript/type.ts
+++ typescript/type.ts
@@ -1,12 +1,37 @@
-input: [1, 2, 3, 4, 5]
-
 type InputArray = number[];
+type OutputArray = number[];
 
-type OutputArray = number[];    
-function processArray(input: InputArray): OutputArray {
-    return input.map(x => x * 2);
+function processArray(arr: InputArray): OutputArray {
+  return arr.map(num => num * 2);
 }
+
 const input: InputArray = [1, 2, 3, 4, 5];
 const output: OutputArray = processArray(input);
-console.log(output); // Output: [2, 4, 6, 8, 10]    
-    
+
+console.log(output); // [2, 4, 6, 8, 10]
+
+
+
+
+type User = {
+  id: number;
+  name: string;
+  email: string;
+};  
+
+const user: User [] = [
+  { id: 1, name: "Alice", age: 30 },
+    { id: 2, name: "Bob",  age: 25 },
+    { id: 3, name: "Charlie",  age: 35 },
+];
+function getUserNames(users: User[]): string[] {
+  return users.map(user => user.name);
+}
+const userNames: string[] = getUserNames(user);
+console.log(userNames); // ["Alice", "Bob", "Charlie"]
+
+
+
+
+
+
