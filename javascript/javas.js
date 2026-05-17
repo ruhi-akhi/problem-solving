39217f91d20595bd67c414726c1d829f1445ad8a
akhiakter25556 <akhiakterherpower80@gmail.com>
2026-05-17 15:17:41 +0600

    javascript problem slove

--- javascript/javas.js
+++ javascript/javas.js
@@ -6,3 +6,9 @@
 // const input = [1, 2, 3, 4, 5];
 // const output = processArray(input);
 // console.log(output); // Output: [2, 4, 6, 8, 10]    
+
+
+for(const fruit of [1, 2, 3, 4, 5]){
+    console.log(fruit);
+    console.log('i want to eat ' + fruit);
+}
