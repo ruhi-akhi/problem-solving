ae1db5d5c1d84c6e1139eed07d697d5db6d6b25a
akhiakter25556 <akhiakterherpower80@gmail.com>
2026-08-10 23:06:44 +0600

    Function to calculate problem slove

--- javascript/javas.js
+++ javascript/javas.js
@@ -8,7 +8,22 @@
 // console.log(output); // Output: [2, 4, 6, 8, 10]    
 
 
-for(const fruit of [1, 2, 3, 4, 5]){
-    console.log(fruit);
-    console.log('i want to eat ' + fruit);
+// for(const fruit of [1, 2, 3, 4, 5]){
+//     console.log(fruit);
+//     console.log('i want to eat ' + fruit);
+// }
+
+
+// Printing PI value
+console.log(Math.PI);
+
+// Function to calculate circle area
+function calculateCircleArea(radius) {
+  let area = Math.PI * radius * radius;
+  return area;
 }
+
+console.log(calculateCircleArea(5));
+console.log(calculateCircleArea(10));
+
+
